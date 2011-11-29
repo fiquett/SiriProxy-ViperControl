b16a64451c3468519422e085623487bd5d0c4e2b
Brandon Fiquett <brandon@fiquett.com>
2011-11-29 08:48:09 -0500

    Updated timeout code

--- lib/siriproxy-vipercontrol.rb
+++ lib/siriproxy-vipercontrol.rb
@@ -2,7 +2,6 @@ require 'cora'
 require 'siri_objects'
 require 'json'
 require 'open-uri'
-require 'timeout'
 
 class SiriProxy::Plugin::ViperControl < SiriProxy::Plugin
   	attr_accessor :url
@@ -10,8 +9,9 @@ class SiriProxy::Plugin::ViperControl < SiriProxy::Plugin
   	def initialize(config = {})
     	self.url = config["url"]
   	end
-
-	retries = 2
+  	
+	status = nil
+	
   	#capture ViperControl status
   	listen_for(/Car.*start/i) { send_command_to_car("remote") }
   	listen_for(/start.*Car/i) { send_command_to_car("remote") }
@@ -32,31 +32,27 @@ class SiriProxy::Plugin::ViperControl < SiriProxy::Plugin
 		say  "One moment while I connect to your vehicle..."
 		Thread.new {
 			begin
-     				Timeout::timeout(8){
-					status = JSON.parse(open(URI("#{self.url}?action=#{viper_command}")).read)
-     				}
-   			rescue Timeout::Error
-     				retries -= 1
-     				if retry > 0
-       					sleep 0.42 and retry
-     				else
-       					raise
-     				end
-   			end
-				if(status["Return"]["ResponseSummary"]["StatusCode"] == 0) #successful
-					say "Viper Connection Successful"
-					if(status["Return"]["Results"]["Device"]["Action"] == "arm")
-						say "Vehicle security engaged!"
-					elsif(status["Return"]["Results"]["Device"]["Action"] == "disarm")
-						say "Vehicle security disabled!"
-					elsif(status["Return"]["Results"]["Device"]["Action"]  == "remote")
-						say "Vehicle ignition has been triggered"
-					elsif(status["Return"]["Results"]["Device"]["Action"]  == "trunk")
-						say "Vehicle trunk has been opened"
-					end
-				else
-					say "Sorry, could not connect to your vehicle."
+  				Timeout::timeout(10) do
+   					status = JSON.parse(open(URI("#{self.url}?action=#{viper_command}")).read)
+  				end
+			rescue Timeout::Error
+  				say "Sorry, connection timed out"
+			end
+				
+			if(status["Return"]["ResponseSummary"]["StatusCode"] == 0) #successful
+				say "Viper Connection Successful"
+				if(status["Return"]["Results"]["Device"]["Action"] == "arm")
+					say "Vehicle security engaged!"
+				elsif(status["Return"]["Results"]["Device"]["Action"] == "disarm")
+					say "Vehicle security disabled!"
+				elsif(status["Return"]["Results"]["Device"]["Action"]  == "remote")
+					say "Vehicle ignition has been triggered"
+				elsif(status["Return"]["Results"]["Device"]["Action"]  == "trunk")
+					say "Vehicle trunk has been opened"
 				end
+			else
+				say "Sorry, could not connect to your vehicle."
+			end
 			
 			request_completed
 		}	
