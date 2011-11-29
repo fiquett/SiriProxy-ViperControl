120b5b3767552bf760d9df3da972da1099014408
Brandon Fiquett <brandon@fiquett.com>
2011-11-29 09:11:09 -0500

    Fixed timeout

--- lib/siriproxy-vipercontrol.rb
+++ lib/siriproxy-vipercontrol.rb
@@ -33,28 +33,28 @@ class SiriProxy::Plugin::ViperControl < SiriProxy::Plugin
 		say  "One moment while I connect to your vehicle..."
 		Thread.new {
 			begin
-  				Timeout::timeout(10) do
+  				Timeout::timeout(20) do
    					status = JSON.parse(open(URI("#{self.url}?action=#{viper_command}")).read)
   				end
 			rescue Timeout::Error
   				say "Sorry, connection timed out"
 			end
-				
-			if(status["Return"]["ResponseSummary"]["StatusCode"] == 0) #successful
-				say "Viper Connection Successful"
-				if(status["Return"]["Results"]["Device"]["Action"] == "arm")
-					say "Vehicle security engaged!"
-				elsif(status["Return"]["Results"]["Device"]["Action"] == "disarm")
-					say "Vehicle security disabled!"
-				elsif(status["Return"]["Results"]["Device"]["Action"]  == "remote")
-					say "Vehicle ignition has been triggered"
-				elsif(status["Return"]["Results"]["Device"]["Action"]  == "trunk")
-					say "Vehicle trunk has been opened"
+			if status	
+				if(status["Return"]["ResponseSummary"]["StatusCode"] == 0) #successful
+					say "Viper Connection Successful"
+					if(status["Return"]["Results"]["Device"]["Action"] == "arm")
+						say "Vehicle security engaged!"
+					elsif(status["Return"]["Results"]["Device"]["Action"] == "disarm")
+						say "Vehicle security disabled!"
+					elsif(status["Return"]["Results"]["Device"]["Action"]  == "remote")
+						say "Vehicle ignition has been triggered"
+					elsif(status["Return"]["Results"]["Device"]["Action"]  == "trunk")
+						say "Vehicle trunk has been opened"
+					end
+				else
+					say "Sorry, could not connect to your vehicle."
 				end
-			else
-				say "Sorry, could not connect to your vehicle."
 			end
-			
 			request_completed
 		}	
 	end
